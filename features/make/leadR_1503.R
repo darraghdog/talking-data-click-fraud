b252cdec12f31eab8bb5f257f85bb90321b37d31
dhanley2 <darragh_hanley@optum.com>
2018-03-15 21:30:35 +0000

    Rolling mean features

--- features/make/leadR_1503.R
+++ features/make/leadR_1503.R
@@ -32,6 +32,10 @@ getLag = function(df, cols_, fname, path){
 path = '~/tdata/data/'
 path = '/Users/dhanley2/Documents/tdata/data/'
 
+############################################
+################ Lead & Lag ################
+############################################
+
 # Write out the <ip, device, os, channel, app> level
 trndf = fread(paste0(path, 'trainvalsmall.csv'))
 fname = "lead_lag_trn_ip_device_os_channel_app_valsmall.gz"
@@ -66,5 +70,33 @@ fname = "lead_lag_tst_ip_device_os_channel.gz"
 cols_ = c("ip", "device", "os", "channel")
 getLag(tstdf, cols_, fname, path)
 
+############################################
+########## Click Rolling Mean ##############
+############################################
 
+# Write out the <ip, device, os, channel, app> level
+trndf = fread(paste0(path, 'trainvalsmall.csv'))
+# fname = "lead_lag_trn_ip_device_os_channel_app_valsmall.gz"
+cols_ = c("device", "os", "app")
+trndf$click_time = fasttime::fastPOSIXct(trndf$click_time)
+trndf$click_sec  = as.numeric(trndf$click_time)
+trndf$click_hr   = hour(trndf$click_time)
+trndf$click_day  = wday(trndf$click_time)
+trndf$click_time = NULL
+trndf = trndf[,c(cols_, "click_sec", "click_hr", "click_day"), with = F]
+trndf[, index := 1:nrow(trndf)]
+setorderv(trndf, c(cols_, "click_sec"))
+trndf[,count := length(click_sec), by = cols_ ]
+trndf[,count_hour := length(click_sec), by = c(cols_, "click_hr", "click_day") ]
+trndf[, click_sec_lead_hr := shift(click_sec, 1, type = "lead") - click_sec, by = c(cols_, "click_hr", "click_day") ]
+trndf[count_hour>=4,rmeanhr4 := roll_mean(click_sec_lead_hr, 4, align = "center"), by = c(cols_, "click_hr", "click_day")]
+trndf[count_hour>=40,rmeanhr40 := roll_mean(click_sec_lead_hr, 40, align = "center"), by = c(cols_, "click_hr", "click_day")]
+
+# Make the bayesian mean of rolling mean
+
+
+?RcppRoll::roll_mean
+trndf
+
+table(is.na(trndf$rmeanhr40))
 
