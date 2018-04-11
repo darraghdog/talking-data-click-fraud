3970e2002f0fbf3db773c199991c330a76495087
dhanley2 <darragh_hanley@optum.com>
2018-04-11 20:20:02 +0100

    Start nnet

--- explore/nnet_1104.py
+++ explore/nnet_1104.py
@@ -80,60 +80,27 @@ print ('final part of preparation....')
 test_df = train_df[len_train:]
 train_df = train_df[:len_train]
 y_train = train_df['is_attributed'].values
-train_df.drop(['click_id', 'click_time','ip','is_attributed'],1,inplace=True)
+# train_df.drop(['click_id', 'click_time','ip','is_attributed'],1,inplace=True)
+train_df.drop(['click_time','ip','is_attributed'],1,inplace=True)
 
 
-
-max_app = np.max([train_df['app'].max(), test_df['app'].max()])+1
-max_ch = np.max([train_df['channel'].max(), test_df['channel'].max()])+1
-max_dev = np.max([train_df['device'].max(), test_df['device'].max()])+1
-max_os = np.max([train_df['os'].max(), test_df['os'].max()])+1
-max_h = np.max([train_df['hour'].max(), test_df['hour'].max()])+1
-max_d = np.max([train_df['day'].max(), test_df['day'].max()])+1
-max_wd = np.max([train_df['wday'].max(), test_df['wday'].max()])+1
-max_qty = np.max([train_df['qty'].max(), test_df['qty'].max()])+1
-max_c1 = np.max([train_df['ip_app_count'].max(), test_df['ip_app_count'].max()])+1
-max_c2 = np.max([train_df['ip_app_os_count'].max(), test_df['ip_app_os_count'].max()])+1
+embids = ['app', 'channel', 'device', 'os', 'hour', 'day', 'wday', 'qty', 'ip_app_count', 'ip_app_os_count']
+# get the max of each code type
+embmaxs = dict((col, np.max([train_df[col].max(), test_df[col].max()])+1) for col in embids)
+# Generator
 def get_keras_data(dataset):
-    X = {
-        'app': np.array(dataset.app),
-        'ch': np.array(dataset.channel),
-        'dev': np.array(dataset.device),
-        'os': np.array(dataset.os),
-        'h': np.array(dataset.hour),
-        'd': np.array(dataset.day),
-        'wd': np.array(dataset.wday),
-        'qty': np.array(dataset.qty),
-        'c1': np.array(dataset.ip_app_count),
-        'c2': np.array(dataset.ip_app_os_count)
-    }
+    X = dict((col, np.array(dataset['app'])) for col in embids)
     return X
-train_df = get_keras_data(train_df)
 
+in_app = Input(shape=[1], name = 'app')
+# Dictionary of inputs
 emb_n = 50
 dense_n = 1000
-in_app = Input(shape=[1], name = 'app')
-emb_app = Embedding(max_app, emb_n)(in_app)
-in_ch = Input(shape=[1], name = 'ch')
-emb_ch = Embedding(max_ch, emb_n)(in_ch)
-in_dev = Input(shape=[1], name = 'dev')
-emb_dev = Embedding(max_dev, emb_n)(in_dev)
-in_os = Input(shape=[1], name = 'os')
-emb_os = Embedding(max_os, emb_n)(in_os)
-in_h = Input(shape=[1], name = 'h')
-emb_h = Embedding(max_h, emb_n)(in_h) 
-in_d = Input(shape=[1], name = 'd')
-emb_d = Embedding(max_d, emb_n)(in_d) 
-in_wd = Input(shape=[1], name = 'wd')
-emb_wd = Embedding(max_wd, emb_n)(in_wd) 
-in_qty = Input(shape=[1], name = 'qty')
-emb_qty = Embedding(max_qty, emb_n)(in_qty) 
-in_c1 = Input(shape=[1], name = 'c1')
-emb_c1 = Embedding(max_c1, emb_n)(in_c1) 
-in_c2 = Input(shape=[1], name = 'c2')
-emb_c2 = Embedding(max_c2, emb_n)(in_c2) 
-fe = concatenate([(emb_app), (emb_ch), (emb_dev), (emb_os), (emb_h), 
-                 (emb_d), (emb_wd), (emb_qty), (emb_c1), (emb_c2)])
+# Build the inputs, embeddings and concatenate them all for each column
+emb_inputs = dict((col, Input(shape=[1], name = col))  for col in embids)
+emb_model  = dict((col, Embedding(embmaxs[col], emb_n)(emb_inputs[col])) for col in embids)
+fe = concatenate([(emb_) for emb_ in emb_model.values()])
+# Rest of the model
 s_dout = SpatialDropout1D(0.2)(fe)
 fl1 = Flatten()(s_dout)
 conv = Conv1D(100, kernel_size=4, strides=1, padding='same')(s_dout)
@@ -142,7 +109,8 @@ concat = concatenate([(fl1), (fl2)])
 x = Dropout(0.2)(Dense(dense_n,activation='relu')(concat))
 x = Dropout(0.2)(Dense(dense_n,activation='relu')(x))
 outp = Dense(1,activation='sigmoid')(x)
-model = Model(inputs=[in_app,in_ch,in_dev,in_os,in_h,in_d,in_wd,in_qty,in_c1,in_c2], outputs=outp)
+model = Model(inputs=emb_inputs.values(), outputs=outp)
+
 
 batch_size = 50000
 epochs = 2
@@ -155,10 +123,13 @@ model.compile(loss='binary_crossentropy',optimizer=optimizer_adam,metrics=['accu
 
 model.summary()
 
+train_df = get_keras_data(train_df)
 class_weight = {0:.01,1:.99} # magic
 model.fit(train_df, y_train, batch_size=batch_size, epochs=2, class_weight=class_weight, shuffle=True, verbose=2)
 del train_df, y_train; gc.collect()
 model.save_weights('imbalanced_data.h5')
+
+
 
 sub = pd.DataFrame()
 sub['click_id'] = test_df['click_id'].astype('int')
@@ -171,6 +142,3 @@ del test_df; gc.collect()
 print("writing....")
 sub.to_csv('imbalanced_data.csv',index=False)
 
-
-# write now i don't use validation set
-# since the data is imbalanced i can't understand how we can separate data the right way
